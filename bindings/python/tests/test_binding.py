364b376794a6a5203cf5f00e641e9748a3638b8e
Wilfred Hughes <me@wilfred.me.uk>
2025-11-15 22:56:41 +0000

    Rebuild bindings

---
+++ bindings/python/tests/test_binding.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+from tree_sitter import Language, Parser
+import tree_sitter_elisp
+
+
+class TestLanguage(TestCase):
+    def test_can_load_grammar(self):
+        try:
+            Parser(Language(tree_sitter_elisp.language()))
+        except Exception:
+            self.fail("Error loading Emacs Lisp grammar")
