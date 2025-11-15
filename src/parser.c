894bb618bead1d09c5f3b7e2bd6763088b7457bc
Wilfred Hughes <me@wilfred.me.uk>
2025-11-15 22:48:47 +0000

    Rebuild parser for latest ABI

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 #endif
 
-#define LANGUAGE_VERSION 14
+#define LANGUAGE_VERSION 15
 #define STATE_COUNT 74
 #define LARGE_STATE_COUNT 35
 #define SYMBOL_COUNT 82
@@ -2492,7 +2492,7 @@ static bool ts_lex(TSLexer *lexer, TSStateId state) {
   }
 }
 
-static const TSLexMode ts_lex_modes[STATE_COUNT] = {
+static const TSLexerMode ts_lex_modes[STATE_COUNT] = {
   [0] = {.lex_state = 0},
   [1] = {.lex_state = 50},
   [2] = {.lex_state = 0},
@@ -6433,6 +6433,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_elisp(void) {
     .state_count = STATE_COUNT,
     .large_state_count = LARGE_STATE_COUNT,
     .production_id_count = PRODUCTION_ID_COUNT,
+    .supertype_count = SUPERTYPE_COUNT,
     .field_count = FIELD_COUNT,
     .max_alias_sequence_length = MAX_ALIAS_SEQUENCE_LENGTH,
     .parse_table = &ts_parse_table[0][0],
@@ -6450,6 +6451,13 @@ TS_PUBLIC const TSLanguage *tree_sitter_elisp(void) {
     .lex_modes = (const void*)ts_lex_modes,
     .lex_fn = ts_lex,
     .primary_state_ids = ts_primary_state_ids,
+    .name = "elisp",
+    .max_reserved_word_set_size = 0,
+    .metadata = {
+      .major_version = 1,
+      .minor_version = 6,
+      .patch_version = 0,
+    },
   };
   return &language;
 }
